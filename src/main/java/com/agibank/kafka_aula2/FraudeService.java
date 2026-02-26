c65a797bb7e544de7a7cfb61ee7a1c2a0530f871
Vinicius Bavaresco Madeira <vinicius.madeira@agi.com.br>
2026-02-26 10:25:53 -0300

    Aplica lógica de fraude com Redis

--- src/main/java/com/agibank/kafka_aula2/FraudeService.java
+++ src/main/java/com/agibank/kafka_aula2/FraudeService.java
@@ -1,40 +1,28 @@
 package com.agibank.kafka_aula2;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.Map;
 
 @Service
 public class FraudeService {
 
-    private final Map<String, LocalDateTime> chavesExistentes;
-    private static final Integer TTL_EM_MINUTOS = 5;
+    private final StringRedisTemplate redisTemplate;
+    private static final int TTL_MINUTOS = 1;
 
-    public FraudeService(Map<String, LocalDateTime> chavesExistentes) {
-        this.chavesExistentes = chavesExistentes;
+    public FraudeService(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
     }
 
     public boolean isFraude(String chave) {
-        LocalDateTime agora = LocalDateTime.now();
-        if (!chavesExistentes.containsKey(chave)) {
-            updateChavesExistentes(chavesExistentes, chave, agora);
+        Boolean exists = redisTemplate.hasKey(chave);
+        if (Boolean.TRUE.equals(exists)) {
+            redisTemplate.expire(chave, Duration.ofMinutes(TTL_MINUTOS)); // renova TTL
+            return true;
+        } else {
+            redisTemplate.opsForValue().set(chave, "1", Duration.ofMinutes(TTL_MINUTOS));
             return false;
         }
-        LocalDateTime momentoDoRegistroDaChave = chavesExistentes.get(chave);
-        Duration duration = Duration.between(momentoDoRegistroDaChave, agora);
-        long minutes = duration.toMinutes();
-        updateChavesExistentes(chavesExistentes, chave, agora);
-        return minutes < TTL_EM_MINUTOS;
     }
-
-    private void updateChavesExistentes(
-            Map<String, LocalDateTime> chavesExistentes,
-            String novaChave,
-            LocalDateTime novoAgora
-    ) {
-        chavesExistentes.put(novaChave, novoAgora);
-    }
-
 }
